688d0a99571a4094761a2fe3470cd72b8b7c050b
Chandana Sudha Madhuri Kandari <madhurikandari5@gmail.com>
2024-03-18 22:48:29 +0530

    configured login view, email view in login vc

--- Trade/View Controllers/LoginViewController.swift
+++ Trade/View Controllers/LoginViewController.swift
@@ -9,11 +9,45 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    
+    private var loginView: LoginView!
+    private var emailView: EmailView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        configureLoginView()
     }
 
+    private func configureLoginView() {
+        loginView = LoginView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height))
+        loginView.fixInView(self.view)
+        loginView.loginViewDelegate = self
+    }
+    
+    func configureEmailView() {
+        emailView = EmailView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height))
+        emailView.fixInView(self.view)
+        emailView.emailViewDelegate = self
+    }
+    
+    private func removeSubViews() {
+        if let view = self.view.subviews.first as? LoginView {
+            view.removeFromSuperview()
+        }
+    }
 
 }
 
+extension LoginViewController: LoginViewDelegate {
+    func didTapSignInEmail(view: LoginView) {
+        removeSubViews()
+        configureEmailView()
+    }
+}
+
+extension LoginViewController: EmailViewDelegate {
+    func didTapBack(view: EmailView) {
+        removeSubViews()
+        configureLoginView()
+    }
+}
