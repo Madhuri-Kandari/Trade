fa55a0b681ed868dd2a24eaccb6f51a36ab340d2
Chandana Sudha Madhuri Kandari <madhurikandari5@gmail.com>
2024-03-27 23:24:43 +0530

    created phone number xib and class and configured it

--- Trade/Views/PhoneNumberView/PhoneNumberView.swift
+++ Trade/Views/PhoneNumberView/PhoneNumberView.swift
@@ -7,14 +7,72 @@
 
 import UIKit
 
+protocol PhoneNumberViewDelegate: AnyObject {
+    func didTapSignInOnPhoneNumberView(view: PhoneNumberView, phoneNumber: String)
+    func didTapBack(view: PhoneNumberView)
+}
+
 class PhoneNumberView: UIView {
+    
+    weak var phoneNumberViewDelegate: PhoneNumberViewDelegate?
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    @IBOutlet weak var backButtonOutlet: UIButton! {
+        didSet {
+            backButtonOutlet.layer.cornerRadius = 20
+        }
     }
-    */
+    
+    @IBOutlet weak var getOTPOutlet: UIButton! {
+        didSet {
+            getOTPOutlet.layer.cornerRadius = 20
+            getOTPOutlet.isEnabled = false
+        }
+    }
+    
+    @IBOutlet weak var phoneNumberTextfieldOutlet: UITextField!
+    
+    
+    @IBAction func getOTPAction(_ sender: UIButton) {
+        guard let text = phoneNumberTextfieldOutlet.text else {
+            return
+        }
+        getOTPOutlet.isEnabled = true
+        phoneNumberViewDelegate?.didTapSignInOnPhoneNumberView(view: self, phoneNumber: text)
+    }
+    @IBAction func backAction(_ sender: UIButton) {
+        phoneNumberViewDelegate?.didTapBack(view: self)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        guard let view = Bundle.main.loadNibNamed("PhoneNumberView", owner: self, options: nil)?.first as? UIView else {
+            fatalError()
+        }
+        view.fixInView(self)
+        phoneNumberTextfieldOutlet.delegate = self
+    }
+    
+    private func validatePhoneNumber() {
+        guard let text = phoneNumberTextfieldOutlet.text, !text.isEmpty else {
+            getOTPOutlet.isEnabled = false
+            return
+        }
+        getOTPOutlet.isEnabled = true
+    }
 
+}
+
+extension PhoneNumberView: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        validatePhoneNumber()
+        return true
+    }
 }
